9c42ba7da4e3efc3014c87edd70149a9a7202499
Steven Kalt <SKalt@users.noreply.github.com>
2017-12-09 20:42:58 -0500

    Fixed shebang

--- text_editing/.atom/import_packages.sh
+++ text_editing/.atom/import_packages.sh
@@ -1,2 +1,2 @@
-#! /usr/bin/env python
+#! /usr/bin/env bash
 apm install linter-ui-default python-tools linter linter-eslint block-comment docblockr busy-signal autoclose-html color-picker css-declaration-sorter linter-stylelint MagicPython language-vue-component project-manager autocomplete-python language-restructuredtext intentions sort-lines language-vue linter-flake8 svn code-peek atom-beautify markdown-preview-plus Hydrogen
