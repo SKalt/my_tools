d41b9d76ecf0e87e3006a9780d0ee442b81a6bde
Steven Kalt <SKalt@users.noreply.github.com>
2017-12-09 20:42:09 -0500

    Fixed exported shebang
    from python (incorrect) to bash

--- text_editing/.atom/export_packages.py
+++ text_editing/.atom/export_packages.py
@@ -5,5 +5,5 @@ import os
 if __name__ == '__main__':
     with open('import_packages.sh', 'w') as f:
         to_export = [i for i in os.listdir('packages') if 'README' not in i]
-        export = '#! /usr/bin/env python\napm install ' + ' '.join(to_export)
+        export = '#! /usr/bin/env bash\napm install ' + ' '.join(to_export)
         f.write(export)
