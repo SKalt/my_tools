90a8fed3306f55f17f8ebe81590adf51693084e0
Steven Kalt <kalt.steven@gmail.com>
2018-10-09 22:02:58 -0400

    etc

--- text_editing/import_packages.sh
+++ text_editing/import_packages.sh
@@ -1,2 +1,36 @@
 #! /usr/bin/env bash
-apm install atom-ide-debugger-python linter-eslint autocomplete-python atom-ide-debugger-node docblock-python block-comment autocomplete-ruby svn code-peek linter-ruby language-vue-component linter-ui-default language-vue autoclose-html atom-beautify ide-json css-declaration-sorter color-picker python-tools project-manager atom-ide-ui linter Hydrogen linter-stylelint markdown-preview-plus docblockr busy-signal linter-flake8 language-restructuredtext ide-yaml ide-vue MagicPython intentions sort-lines
+apm install \
+  Hydrogen \
+  MagicPython \
+  atom-beautify \
+  atom-ide-debugger-node \
+  atom-ide-debugger-python \
+  atom-ide-ui \
+  autoclose-html \
+  autocomplete-python \
+  autocomplete-ruby \
+  block-comment \
+  busy-signal \
+  code-peek \
+  color-picker \
+  css-declaration-sorter \
+  docblock-python \
+  docblockr \
+  ide-json \
+  ide-vue \
+  ide-yaml \
+  intentions \
+  language-restructuredtext \
+  language-vue \
+  language-vue-component \
+  linter \
+  linter-eslint \
+  linter-flake8 \
+  linter-ruby \
+  linter-stylelint \
+  linter-ui-default \
+  markdown-preview-plus \
+  project-manager \
+  python-tools \
+  sort-lines \
+  svn
